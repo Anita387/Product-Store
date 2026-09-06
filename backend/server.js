e87123384fab1f95c24d7e7fc57efeba501cbf34
Anita387 <fsydy310@gmail.com>
2026-09-06 15:36:54 +0330

    sending a request to front

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 //const express = require('express');
 
-
 import express from 'express';
 const app = express();
 
+app.get("/product",(reg ,res) => {
+    res.send("server is ready");
+});
+
 app.listen(5000,()=>{
-    console.log ("server started at https://localhost:5000")
+    console.log ("server started at http://localhost:5000")
 });
