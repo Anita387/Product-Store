edc1491caa590f972ffcb4036c3138d7e7e93456
Anita387 <fsydy310@gmail.com>
2026-09-06 15:19:20 +0330

    starting the server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 //const express = require('express');
 
+
 import express from 'express';
+const app = express();
+
+app.listen(5000,()=>{
+    console.log ("server started at https://localhost:5000")
+});
