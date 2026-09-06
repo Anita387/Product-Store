e8764a43af3ddbb53f92ccc16f57be30dea34e71
Anita387 <fsydy310@gmail.com>
2026-09-06 16:06:54 +0330

    connectig to our database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 //const express = require('express');
-
 import express from 'express';
+//conecting to database 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 app.get("/product",(reg ,res) => {
     res.send("server is ready");
 });
+
+console.log(process.env.MONGO_URI)
 
 app.listen(5000,()=>{
     console.log ("server started at http://localhost:5000")
