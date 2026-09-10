533b2a86d923c2df8721f3481e7975c144b53e2c
Anita387 <fsydy310@gmail.com>
2026-09-10 14:08:04 +0330

    connecting to chakra for front dev

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
